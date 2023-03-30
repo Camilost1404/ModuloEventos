82ef082c25633b35fe50550108b654887bd28a10
Andrés Torres <camitorres1404@gmail.com>
2023-03-29 21:39:59 -0500

    Modelo Evento creado

--- Evento/models.py
+++ Evento/models.py
@@ -11,9 +11,29 @@ class Programa(models.Model):
     codigo_programa = models.CharField(max_length=15)
 
     class Meta:
-        
-        ## Para no hacer cambios en los atributos a la base de datos
+
+        # Para no hacer cambios en los atributos a la base de datos
         managed = False
-        
-        ## Para cambiar el nombre por defecto de la tabla (Poner el nombre que hay establecido en la BD)
+
+        # Para cambiar el nombre por defecto de la tabla (Poner el nombre que hay establecido en la BD)
         db_table = "programa"
+
+
+class Evento(models.Model):
+
+    # Definición de los valores del atributo ENUM
+
+    class EstadoEvento(models.TextChoices):
+
+        APROBADO = 'Aprobado'
+        DENEGADO = 'No Aprobado'
+        PROGRESO = 'En proceso'
+
+    idEvento = models.IntegerField(primary_key=True)
+    Administrativo_idAdministrativo = ... ## models.ForeignKey(Administrativo, on_delete=models.CASCADE)
+    nombre_evento = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=150)
+    fecha_inicio = models.DateTimeField()
+    fecha_final = models.DateTimeField()
+    estado = models.CharField(max_length=15, choices=EstadoEvento.choices, default=EstadoEvento.PROGRESO)
+    correccion = models.CharField(max_length=150, null=True, blank=False)
