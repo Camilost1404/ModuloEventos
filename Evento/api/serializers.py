97eb33a0bcab711213d4dba411814ff1ea78db63
Andrés Torres <camitorres1404@gmail.com>
2023-03-29 22:34:18 -0500

    Serializador para crear evento

--- Evento/api/serializers.py
+++ Evento/api/serializers.py
@@ -1,3 +1,20 @@
 from rest_framework import serializers
+from Evento.models import Evento
 
-## Agregar los serializadores
+# Agregar los serializadores
+
+
+class EventoCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Evento
+        fields = [
+            'idEvento',
+            'Administrativo_idAdministrativo',
+            'nombre_evento',
+            'descripcion',
+            'fecha_inicio',
+            'fecha_final',
+            'estado'
+        ]
