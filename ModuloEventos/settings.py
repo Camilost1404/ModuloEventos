9d34392b4393a6f5953e95d2a2c90c974538ffa8
Andrés Torres <camitorres1404@gmail.com>
2023-03-29 22:10:34 -0500

    Ultimos cambios que se necesitan

--- ModuloEventos/settings.py
+++ ModuloEventos/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Evento'
+    'Evento',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
