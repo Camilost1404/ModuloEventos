23d6e999342217abc8ae0ab425f168240fe21fb3
Andrés Torres <camitorres1404@gmail.com>
2023-03-29 19:51:36 -0500

    Agregación db_table

--- Evento/models.py
+++ Evento/models.py
@@ -11,4 +11,9 @@ class Programa(models.Model):
     codigo_programa = models.CharField(max_length=15)
 
     class Meta:
+        
+        ## Para no hacer cambios en los atributos a la base de datos
         managed = False
+        
+        ## Para cambiar el nombre por defecto de la tabla (Poner el nombre que hay establecido en la BD)
+        db_table = "programa"
